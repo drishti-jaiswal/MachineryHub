95b8fe2c5a1c1bfc23455b87d99d212c372f7797
DRISHTI JAISWAL <147738121+drishti-jaiswal@users.noreply.github.com>
2025-04-13 10:32:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Welcome to Machinery Marketplace!');
+    // You can add more interactive features here
+});
